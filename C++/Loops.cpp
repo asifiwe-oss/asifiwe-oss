3584ad0e09031a96287b982d22309f2a36f8f5c1
asifiwe <mucyoasifiwe80@gmail.com>
2026-01-17 20:21:31 +0200

    loops..

--- C++/Loops.cpp
+++ C++/Loops.cpp
@@ -6,15 +6,45 @@ int main(){
 
     for(int i = 1; i <= 5; i++){
         cout << i << endl;
+        
     }
 
     int n;
     cout << "How many numbers do you want to print?" <<endl;
     cin>>n;
     for(int i = 0; i <= n; i++){
-        cout << i << " ";
+        cout << i << " " <<endl;
     }
     //while loop
+
+    int o;
+    int a = 1;
+    cout << "Enter o" <<endl;
+    cin >> o;
+    while (a <= o){
+    cout << a <<endl;
+    a += 1;
+    }
+   
+
+    int x;
+        cout << "Enter a number: " <<endl;
+    cin>> x;
+    while( x != 0){
+        cout << "Enter a number: " <<endl;
+        cin >> x;
+    } 
+   cout <<"Finished!" <<endl; 
+
+   int m;
+   int sum = 0;
+      cout << "Enter a numbers: "<<endl;
+      cin >> m;
+      while( m != 0){
+      cout << "Enter a number: " <<endl;
+      sum += m;
+      cin >> m;               } 
+      cout <<"Sum = " <<sum;
 
     return 0;
 }
